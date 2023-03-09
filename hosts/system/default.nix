eb90a1b2716ae67a482df1412139bd36cf9ac631
arrows-of-time <arrows-of-time@proton.me>
2023-03-09 20:36:44 +0100

    Update structure

---
+++ hosts/system/default.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./boot.nix
+    ] ++ [
+      ./locale.nix
+    ] ++ [
+      ./sound.nix
+    ] ++ [
+      ./networking.nix
+    ];
+
+  # basic
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "22.11";
+
+  # flakes
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # nix
+  nix = {
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 5d";
+    };
+  };
+
+  # basic programs
+  environment.systemPackages = with pkgs; [
+    nano
+    git
+    gnupg
+  ];
+
+  # gnupg
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryFlavor = "gtk2";
+    enableSSHSupport = true;
+  };
+
+  # console
+  console = {
+    earlySetup = true;
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
+    packages = with pkgs; [ terminus_font ];
+    keyMap = "de";
+  };
+}
